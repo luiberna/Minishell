0df8b1ba7ff0c28a16db69f4326d36d4346de3ba
luiberna <146244524+luiberna@users.noreply.github.com>
2024-06-25 20:50:53 +0100

    Add files via upload

--- expander.c
+++ expander.c
@@ -6,30 +6,70 @@
 /*   By: luiberna <luiberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 17:18:53 by luiberna          #+#    #+#             */
-/*   Updated: 2024/06/24 18:31:42 by luiberna         ###   ########.fr       */
+/*   Updated: 2024/06/25 20:48:33 by luiberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *ft_strjoin_free(char *s1, char *s2)
+char    *create_final_str(char *start_str, char *env_value, char *end_str)
 {
-    char *result = ft_strjoin(s1, s2);
-    free(s1);
-    free(s2);
-    return result;
+    char *temp_str1;
+    char *temp_str2;
+
+    if (start_str)
+    {
+        if (env_value)
+            temp_str1 = ft_strjoin(start_str, env_value);
+        else
+            temp_str1 = ft_strjoin(start_str, "");
+    }
+    else
+    {
+        if (env_value)
+            temp_str1 = ft_strdup(env_value);
+        else
+            temp_str1 = ft_strdup("");
+    }
+    if (end_str)
+    {
+        temp_str2 = ft_strjoin(temp_str1, end_str);
+        free(temp_str1);
+    }
+    else
+        temp_str2 = temp_str1;
+    return (temp_str2);
 }
 
-char *get_env_value(char *name, t_env *env)
+char    *get_end_str(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i] && str[i] != ' ' && str[i] != '\"')
+        i++;
+    if (str[i])
+        return (&str[i]);
+    else
+        return (NULL);
+}
+
+char    *get_env_value(char *str, t_env *env)
 {
     int i;
     int name_len;
 
     i = 0;
-    name_len = ft_strlen(name);
+    name_len = 0;
+    while (str[i] != ' ' && str[i] && str[i] != '\"')
+    {
+        name_len++;
+        i++;
+    }
+    i = 0;
     while (env->envp[i])
     {
-        if (ft_strncmp(name, env->envp[i], name_len) == 0 && env->envp[i][name_len] == '=')
+        if (ft_strncmp(str, env->envp[i], name_len) == 0 && env->envp[i][name_len] == '=')
             return (&env->envp[i][name_len + 1]);
         i++;
     }
@@ -59,43 +99,12 @@ char    *get_start_str(char *str)
     return (temp_str);
 }
 
-char    *create_final_str(char *start_str, char *env_value, char *end_str)
-{
-    char *temp_str1;
-    char *temp_str2;
-
-    if (start_str)
-    {
-        if (env_value)
-            temp_str1 = ft_strjoin_free(start_str, env_value);
-        else
-            temp_str1 = ft_strjoin_free(start_str, "");
-        free (start_str);
-    }
-    else
-    {
-        if (env_value)
-            temp_str1 = ft_strdup(env_value);
-        else
-            temp_str1 = ft_strdup("");
-    }
-    if (end_str)
-    {
-        temp_str2 = ft_strjoin_free(temp_str1, end_str);
-        free(end_str);
-        free(temp_str1);
-    }
-    else
-        temp_str2 = temp_str1;
-    return (temp_str2);
-}
-
-char    *expand_with_quotes(char *str, t_env *env)
+char    *expand(char *str, t_env *env)
 {
     int i;
     char *start_str;
-    char *end_str;
     char *env_value;
+    char *end_str;
     char *temp_str;
 
     i = 0;
@@ -105,16 +114,9 @@ char    *expand_with_quotes(char *str, t_env *env)
         {
            start_str = get_start_str(str);
            env_value = get_env_value(&str[i + 1], env);
-           while (str[i] != ' ' || str[i] != '\0')
-                i++;
-           if (str[i] == '\0')
-                end_str = NULL;
-           else
-                end_str = ft_strdup(&str[i]);
+           end_str = get_end_str(&str[i]);
            temp_str = create_final_str(start_str, env_value, end_str);
-           free(end_str);
-           free(str);
-           str = ft_strdup(temp_str);
+           str = temp_str;
            i = 0;
         }
         i++;
@@ -125,7 +127,7 @@ char    *expand_with_quotes(char *str, t_env *env)
 void    expander(t_cmd *cmd, t_env *env)
 {
     int i;
-    char *env_value;
+    int j;
     char *expanded_str;
 
     while (cmd)
@@ -133,21 +135,17 @@ void    expander(t_cmd *cmd, t_env *env)
         i = 0;
         while (cmd->cmd[i])
         {
-            if (cmd->cmd[i][0] == '$')
+            j = -1;
+            while (cmd->cmd[i][++j])
             {
-                env_value = get_env_value(cmd->cmd[i] + 1, env);
-                free(cmd->cmd[i]);
-                if (env_value)
-                    cmd->cmd[i] = ft_strdup(env_value);
-                else
-                    cmd->cmd[i] = ft_strdup("");
-            }
-            else if (cmd->cmd[i][0] == '\"')
-            {
-                expanded_str = expand_with_quotes(cmd->cmd[i], env);
-                free(cmd->cmd[i]);
-                cmd->cmd[i] = ft_strdup(expanded_str);
-                free(expanded_str);
+                if (cmd->cmd[i][0] == '\'')
+                    break;
+                if (cmd->cmd[i][j] == '$')
+                {
+                    expanded_str = expand(cmd->cmd[i], env);
+                    free(cmd->cmd[i]);
+                    cmd->cmd[i] = ft_strdup(expanded_str);
+                }
             }
             i++;
         }
